8cf66297dde2e8581f8ccd855454e2c16ef9ad93
airin181 <98608110+airin181@users.noreply.github.com>
2022-03-28 23:59:03 +0200

    UPDATE: connections db

--- models/dbconfig-pg.js
+++ models/dbconfig-pg.js
@@ -3,7 +3,7 @@ const env = require("dotenv").config();
 const { Pool } = require('pg')
 const pool = new Pool({
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.USER_DB,
     database: process.env.DATABASE,
     password: process.env.PASSWORD
 })
