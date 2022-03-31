3f6504f1409ebeb5ee66232b4037ba88790d98c9
airin181 <98608110+airin181@users.noreply.github.com>
2022-03-31 17:54:38 +0200

    testing new column admin

--- utils/queries.js
+++ utils/queries.js
@@ -2,11 +2,11 @@
 
 const signUpUserQuery = `INSERT INTO users(name,surname,email,password) VALUES ($1,$2,$3,$4)`
 
-/* const logInQuery = `INSERT INTO users(name,surname,email,password) VALUES ($1,$2,$3,$4)`
+/* const loginUserQuery= `SELECT * FROM users (email, password) WHERE email = $1 && password = $2`
  */
 const queries = {
 signUpUserQuery,
-/* logInQuery */
+/* loginUserQuery */
 }
 
 
