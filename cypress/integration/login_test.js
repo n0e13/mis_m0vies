9b6c44ceac969eace56a6c0e78da3eaf5975ca1a
francospatz <francospatzdilorenzo@gmail.com>
2022-04-08 04:20:26 +0200

    Testing last commit

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -5,7 +5,7 @@ describe('Login form test with incorrect parms', () => {
         cy.url().should('include', 'login')
         cy.url().should('eq', (Cypress.env('URL_BASE') + "login"))
         cy.get('[type="text"]').type("testuser@gmail.com")
-        cy.get('[type="password"]').type("Testuser-0")
+        cy.get('[type="password"]').type("aslkjf32--gre23")
         cy.get('form').submit()
         cy.get('body').should('have.text', 'Email or password incorrect')
     })
