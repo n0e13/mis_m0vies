f2ff4d7d34c332bfc571dcc7d6217ddb8b8b99fb
francospatz <francospatzdilorenzo@gmail.com>
2022-04-08 04:10:29 +0200

    .gitignote now ignores .env.json

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -1,9 +1,12 @@
-describe('Login form test', () => {
-    it.only('Login scenario', () => {
+describe('Login form test with incorrect parms', () => {
+    it('Login scenario', () => {
         cy.visit(Cypress.env('URL_BASE'))
+        cy.get('#login-home').click()
+        cy.url().should('include', 'login')
+        cy.url().should('eq', (Cypress.env('URL_BASE') + "login"))
         cy.get('[type="text"]').type("testuser@gmail.com")
-        cy.get('[type="password"]').type("Testuser-123")
+        cy.get('[type="password"]').type("Testuser-0")
         cy.get('form').submit()
-        
+        cy.get('body').should('have.text', 'Email or password incorrect')
     })
 })
